d26401341d5124878a2764f9414f2acdbb27f105
Kyle Schang <kyle@schang.net>
2024-05-28 14:26:36 -0400

    added the project

---
+++ QuickerTipper/QuickerTipper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  QuickerTipper
+//
+//  Created by Kyle Schang on 5/28/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ QuickerTipper/QuickerTipper/QuickerTipperApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickerTipperApp.swift
+//  QuickerTipper
+//
+//  Created by Kyle Schang on 5/28/24.
+//
+
+import SwiftUI
+
+@main
+struct QuickerTipperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
